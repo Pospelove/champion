f75004b24dab9b56f3db8e3c667c74ff96923dd5
Leonid Pospelov <pospelovlm@yandex.ru>
2022-11-10 03:38:53 +0400

    Remove comment

--- utils_path.go
+++ utils_path.go
@@ -3,14 +3,6 @@ package main
 import "strings"
 
 func GetLastFileExtension(path string) string {
-	//tokens := strings.Split(path, ".")
-	// If there is no extension, return empty string
-	// In case of a file like ".gitignore", return empty string
-	//if len(tokens) == 0 {
-	//		return ""
-	//	}
-	//	return tokens[len(tokens)-1]
-
 	tokens := strings.Split(path, ".")
 	if len(tokens) <= 1 {
 		return ""
